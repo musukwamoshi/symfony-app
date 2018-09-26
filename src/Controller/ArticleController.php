ce7bf72cb949f7fca2d514ad6c721199c0ef6115
Moshi Musukwa <musukwamoshi@gmail.com>
2018-09-26 19:55:35 +0200

    Added an index view

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController {
+
+class ArticleController extends AbstractController {
 //note that for annotations use double quotes
 
 /**
@@ -14,7 +16,8 @@ class ArticleController {
  */
 public function index(){
 
-  return new Response('Hello World');
+  //return new Response('Hello World');
+  return $this->render('articles/index.html.twig');
 
 }
 
